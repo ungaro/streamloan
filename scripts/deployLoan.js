bef554c96156a9b3d59ba938d208064c1bb8a2c1
Kai Morciniec <kaimorciniec@gmail.com>
2022-11-06 11:18:52 -0800

    add initial deployments

--- scripts/deployLoan.js
+++ scripts/deployLoan.js
@@ -1,67 +1,62 @@
-const ethers = require("ethers")
-const { Framework } = require("@superfluid-finance/sdk-core")
+const ethers = require("ethers");
+const { Framework } = require("@superfluid-finance/sdk-core");
 const LoanFactoryABI =
-    require("../artifacts/contracts/LoanFactory.sol/LoanFactory.json").abi
-require("dotenv").config()
+  require("../artifacts/contracts/LoanFactory.sol/LoanFactory.json").abi;
+require("dotenv").config();
 
 //place deployed address of the loan factory here...
-const LoanFactoryAddress = "0x7b5655c12Fc4fceCCD8C3e144c413a0Ae5dc8DbA"
+const LoanFactoryAddress = "0x56e173518E9Cf3778aAC908399836014873bF182";
 
 //NOTE: this is set as the goerli url, but can be changed to reflect your RPC URL and network of choice
-const url = process.env.GOERLI_URL
+const url = process.env.GOERLI_URL;
 
-const customHttpProvider = new ethers.providers.JsonRpcProvider(url)
+const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+const stIbAlluoUSD = "0x2efc02e2cdcc1ef699f4af7e98b20f8a2a30923d"; //StIbAlluoUSD on mumbai
 
 async function main() {
-    const network = await customHttpProvider.getNetwork()
+  const network = await customHttpProvider.getNetwork();
 
-    const sf = await Framework.create({
-        chainId: network.chainId,
-        provider: customHttpProvider
-    })
+  const sf = await Framework.create({
+    chainId: network.chainId,
+    provider: customHttpProvider,
+  });
 
-    const borrower = sf.createSigner({
-        privateKey: process.env.BORROWER_PRIVATE_KEY,
-        provider: customHttpProvider
-    })
+  const borrower = sf.createSigner({
+    privateKey: process.env.BORROWER_PRIVATE_KEY,
+    provider: customHttpProvider,
+  });
 
-    const employer = sf.createSigner({
-        privateKey: process.env.EMPLOYER_PRIVATE_KEY,
-        provider: customHttpProvider
-    })
+  const employer = sf.createSigner({
+    privateKey: process.env.EMPLOYER_PRIVATE_KEY,
+    provider: customHttpProvider,
+  });
 
-    const daix = await sf.loadSuperToken("fDAIx") //get fDAIx on goerli
+  const loanFactory = new ethers.Contract(
+    LoanFactoryAddress,
+    LoanFactoryABI,
+    customHttpProvider
+  );
 
-    const loanFactory = new ethers.Contract(
-        LoanFactoryAddress,
-        LoanFactoryABI,
-        customHttpProvider
+  await loanFactory
+    .connect(borrower)
+    .createNewLoan(
+      ethers.utils.parseEther("10"), //borrow amount = 1000 dai
+      30, //24 days payback period
+      employer.address, //address of employer who will be effectively whitelisted in this case
+      borrower.address, // address of borrower
+      stIbAlluoUSD.address, //StIbAlluoUSD address - this is the token used for the stream payment
+      sf.settings.config.hostAddress //address of host
     )
-
-    await loanFactory
-        .connect(borrower)
-        .createNewLoan(
-            ethers.utils.parseEther("1000"), //borrow amount = 1000 dai
-            8, // 8% interest rate
-            24, //24 months payback period
-            employer.address, //address of employer who will be effectively whitelisted in this case
-            borrower.address, // address of borrower
-            daix.address, //daix address - this is the token we'll be using: borrowing in and paying back
-            sf.settings.config.hostAddress //address of host
-        )
-        .then(tx => {
-            console.log(
-                "deployment successful! here is your tx hash: ",
-                tx.hash
-            )
-        })
+    .then((tx) => {
+      console.log("deployment successful! here is your tx hash: ", tx.hash);
+    });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
